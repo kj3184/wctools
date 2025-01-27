7af72e90ac8be52e6a5f5525c87f0be593cd39f1
kj3184 <kunal.jadhav84@gmail.com>
2025-01-27 22:29:47 +0800

    Modified countbytes function

--- wc_tool.py
+++ wc_tool.py
@@ -1,42 +1,45 @@
-import re
 import os
+import re
+
 
 def countlines(filename):
     try:
-        with open(filename,"r") as textfile:
+        with open(filename, "r") as textfile:
             lines = textfile.readlines()
             return len(lines)
     except FileNotFoundError:
-        print("No such file "+filename +" found")
+        print("No such file " + filename + " found")
         return False
 
+
 def countbytes(filename):
-    try:
-        character_count=0
-        with open(filename, 'r') as filedata:
-            character_count=os.stat(filename).st_size
-            return character_count
-    except FileNotFoundError:
-        print("No such file "+filename +" found")
+    bytes_count = 0
+    if os.path.isfile(filename):
+        bytes_count = os.stat(filename).st_size
+        return bytes_count
+    else:
+        print("No such file " + filename + " found")
         return False
+
 
 def countchars(filename):
     try:
-        character_count=0
-        with open(filename,"r") as filedata:
-            text=filedata.read()
+        character_count = 0
+        with open(filename, "r") as filedata:
+            text = filedata.read()
             # Remove whitespace characters
             character_count = len(re.sub(r"\s+", "", text, flags=re.UNICODE))
             return character_count
     except FileNotFoundError:
-        print("No such file "+filename +" found")
+        print("No such file " + filename + " found")
         return False
+
 
 def countwords(filename):
     try:
-        with open(filename,"r") as textfile:
-            text=textfile.read()
-            words = re.findall(r'\b\w+\b', text)
+        with open(filename, "r") as textfile:
+            text = textfile.read()
+            words = re.findall(r"\b\w+\b", text)
             return len(words)
     except FileNotFoundError:
         print("No such file " + filename + " found")
